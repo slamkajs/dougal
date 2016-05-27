22b7cd523bb21ebc2e5d4ab8979b2f2c112f8721
Emmanuel Bourgerie <manu@bourgerie.fr>
2016-05-27 18:05:31 +0100

    Validation code reworked and new save functions

--- dougal.js
+++ dougal.js
@@ -8,8 +8,8 @@
   angular.module('dougal', [])
     .factory('Model', ModelFactory);
 
-  ModelFactory.$inject = ['$q'];
-  function ModelFactory($q) {
+  ModelFactory.$inject = ['$http', '$interpolate', '$q'];
+  function ModelFactory($http, $interpolate, $q) {
 
     /**
      * (soon)
@@ -21,6 +21,16 @@
      * * `$get`
      * * `$set`
      * * `$validate`
+     *
+     * `baseUrl` (String)
+     *
+     * | Action | URL | Method |
+     * |--------|-----|--------|
+     * | Index | /cars | GET |
+     * | Fetch | /cars/:id | GET |
+     * | Create | /cars | POST |
+     * | Update | /cars/:id | PUT |
+     * | Delete | /cars/:id | DELETE |
      *
      * `idAttribute` (String) overrides the attribute used for {@link Model#$id}
      *
@@ -39,8 +49,16 @@
       }
 
       ExtendedModel.prototype = _.create(Model.prototype, {
+        $$idAttribute: options.idAttribute || 'id',
         $$options: options
       });
+
+      if (options.baseUrl) {
+        ExtendedModel.prototype.$$baseUrl = {
+          index: $interpolate(options.baseUrl),
+          fetch: $interpolate(options.baseUrl + '/{{' + ExtendedModel.prototype.$$idAttribute + '}}')
+        };
+      }
 
       _.each(options.attributes, function (attribute, key) {
         Object.defineProperty(ExtendedModel.prototype, key, {
@@ -62,6 +80,30 @@
       });
 
       return ExtendedModel;
+    };
+
+    /**
+     * (soon)
+     *
+     * @static
+     * @memberof module:dougal
+     * @param method {String}
+     * @param model {module:dougal.Model}
+     * @returns {Promise}
+     * @since NEXT_VERSION
+     */
+    Model.sync = function (method, model) {
+      var options = {
+        data: model.$toJson(),
+        url: model.$url()
+      };
+
+      options.method = {
+        create: 'POST',
+        update: 'PUT'
+      }[method];
+
+      return $http(options);
     };
 
     /**
@@ -105,8 +147,7 @@
       this.$$previousValues = {};
       this.$$values = {};
 
-      _.assign(this.$$values, values);
-      this.$reset();
+      this.$parse(values);
     }
 
     Model.prototype = {
@@ -127,15 +168,15 @@
       $clear: function () {
         this.$$changed = {};
         this.$$values = _.clone(this.$$previousValues, true);
+        this.$errors = {};
         this.$pristine = true;
-        this.$validate();
       },
 
       /**
-       * Returns the current value of an attribute from the model.
+       * @returns current value of an attribute from the model
        *
-       * @param key
-       * @example car.get('name')
+       * @param key {String}
+       * @example car.$get('name')
        * @since 0.1.0
        */
       $get: function (key) {
@@ -143,11 +184,9 @@
       },
 
       /**
-       * Returns true only if the value was changed, and is now invalid. This function was designed for form validation,
-       * where we do not want to spam the user with error messages until they actually change any value.
+       * @returns {boolean} true if the value is invalid
        *
        * @param key
-       * @returns {boolean}
        * @example
        * var car = new Car({name: ''});
        * car.$hasError('name'); // false
@@ -156,20 +195,20 @@
        * @since 0.1.0
        */
       $hasError: function (key) {
-        return angular.isDefined(this.$$changed[key]) && angular.isDefined(this.$errors[key]);
+        return angular.isDefined(this.$errors[key]);
       },
 
       /**
-       * Returns a unique ID for that model. By default, returns the `id` attribute, but can be overriden by the
-       * `idAttribute` option in [extend]{@link Model.extend}.
+       * @returns a unique ID for that model. By default, the `id` attribute, but can be overriden by the `idAttribute`
+       * option in [extend]{@link Model.extend}.
        * @since 0.1.1
        */
       $id: function () {
-        return this.$$values[this.$$options.idAttribute || 'id'];
+        return this.$$values[this.$$idAttribute];
       },
 
       /**
-       * @returns {boolean}
+       * @returns {boolean} true if the model has an ID
        * @example
        * new Car({}).$isNew();        // true
        * new Car({id: 123}).$isNew(); // false
@@ -190,6 +229,17 @@
       },
 
       /**
+       * (soon)
+       *
+       * @param values
+       * @since NEXT_VERSION
+       */
+      $parse: function (values) {
+        _.assign(this.$$values, values);
+        this.$reset();
+      },
+
+      /**
        * Saves the current state as the last known.
        *
        * @see {@link module:dougal.Model#$clear|$clear()}
@@ -204,15 +254,37 @@
       $reset: function () {
         this.$$changed = {};
         this.$$previousValues = _.clone(this.$$values, true);
+        this.$errors = {};
         this.$pristine = true;
-        this.$validate();
+      },
+
+      /**
+       * (soon)
+       *
+       * @returns {Promise}
+       * @since NEXT_VERSION
+       */
+      $save: function () {
+        return this.$validate()
+          .then(_.bind(function () {
+            function callback(response) {
+              this.$parse(response.data);
+              return this;
+            }
+
+            if (this.$isNew()) {
+              return Model.sync('create', this).then(_.bind(callback, this));
+            } else {
+              return Model.sync('update', this).then(_.bind(callback, this));
+            }
+          }, this));
       },
 
       /**
        * Set the value of an attribute from the model. Changing any value will set `$pristine` to false, even if you
        * revert to its original value.
        *
-       * @param key
+       * @param key {String}
        * @param value
        * @example car.$set('name', 'Super Car!')
        * @since 0.1.0
@@ -225,10 +297,39 @@
       },
 
       /**
+       * @returns {object} a copy of the object attributes
+       * @example
+       * new Car({id: 123, name: 'Super Car!', color: 'blue'}).toJson();
+       * // {id: 123, name: 'Super Car!', color: 'blue'}
+       * @since NEXT_VERSION
+       */
+      $toJson: function () {
+        return _.pick(this.$$values, _.keys(this.$$options.attributes));
+      },
+
+
+      /**
+       * @returns {String}
+       * @example
+       * car = new Car();
+       * car.$url(); // /cars
+       * car = new Car({id: 123});
+       * car.$url(); // /cars/123
+       * @since NEXT_VERSION
+       */
+      $url: function () {
+        if (this.$isNew()) {
+          return this.$$baseUrl.index(this);
+        } else {
+          return this.$$baseUrl.fetch(this);
+        }
+      },
+
+      /**
        * Validates the values of the model
        *
        * @param options
-       * @returns {*}
+       * @returns {Promise}
        * @since 0.1.0
        */
       $validate: function (options) {
@@ -238,7 +339,9 @@
 
         return $q.all(_.map(this.$$options.attributes, _.bind(function (value, key) {
           return this.$$validateAttribute(key);
-        }, this)));
+        }, this))).catch(_.bind(function () {
+          return $q.reject(this.$errors);
+        }, this));
       },
 
       $$validateAttribute: function (key) {
@@ -249,6 +352,7 @@
           }, this))
           .catch(_.bind(function (errorMessage) {
             this.$errors[key] = errorMessage;
+            return $q.reject(errorMessage);
           }, this))
           .finally(_.bind(function () {
             this.$valid = _.isEmpty(this.$errors);
