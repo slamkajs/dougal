d85cea4bbbac878e54282995531fef011d10281a
Emmanuel Bourgerie <manu@bourgerie.fr>
2016-05-31 14:01:02 +0100

    Additional gulp tasks for dev purposes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
+    eslint = require('gulp-eslint'),
     Server = require('karma').Server,
+    shell = require('gulp-shell'),
     wrap = require('gulp-wrap');
 
 var paths = {
@@ -11,14 +13,35 @@ var paths = {
     'src/model.js',
     'src/httpStore.js'
   ],
-  dist: 'dougal.js'
+  dist: 'dougal.js',
+  tests: 'test/*-spec.js'
 };
+
+gulp.task('default', ['doc', 'lint:unsafe', 'dist'], function () {
+  gulp.start('watch:test');
+  return gulp.watch(paths.src, ['doc', 'lint:unsafe', 'dist']);
+});
 
 gulp.task('dist', function () {
   return gulp.src(paths.src)
     .pipe(concat(paths.dist))
     .pipe(wrap({src: 'src/wrap.js.tpl'}))
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('doc', shell.task('./node_modules/.bin/jsdoc --configure jsdoc.conf.json --destination doc'));
+
+gulp.task('lint', function () {
+  return gulp.src(paths.src)
+    .pipe(eslint('./.eslintrc'))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint:unsafe', function () {
+  return gulp.src(paths.src)
+    .pipe(eslint('./.eslintrc'))
+    .pipe(eslint.format());
 });
 
 gulp.task('test', function (done) {
