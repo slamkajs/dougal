89cec2fb06f3c7802d2d610b1c1a67e98ecda7a4
Emmanuel Bourgerie <manu@bourgerie.fr>
2016-06-22 13:54:52 +0100

    Additional documentation

--- src/extend.js
+++ src/extend.js
@@ -6,19 +6,51 @@ function extendModel(Collection, HttpStore, Model) {
   /**
    * Creates a new model that inherits from the {@link module:dougal.Model|Model} class.
    *
+   * #### Attributes properties
+   * **`$get`** custom getter for the attribute. The `$super` parameter is for convenient overriding.
+   *
+   * ```javascript
+   * $get: function ($super) {
+   *   return '+' + $super() + '+';
+   * }
+   * ```
+   *
+   * **`$set`** custom setter for the attribute. The `$super` parameter is for convenient overriding.
+   *
+   * ```javascript
+   * $set: function (value, $super) {
+   *   $super(_.trim(value));
+   * }
+   * ```
+   *
+   * **`$validate`** validation method for the attribute.
+   * For complex validations, can return a promise.
+   * For simple validations, can return `true` or a string/an object if not.
+   *
+   * ```javascript
+   * $validate: function (value) {
+   *   return someCheck(value) ? true : 'The value is invalid because <reason>';
+   *   // or
+   *   var deferred = $q.defer();
+   *   if (someCheck(true)) {
+   *     deferred.resolve();
+   *   } else {
+   *     deferred.reject('The value is invalid because <reason>');
+   *   }
+   *   return deferred.promise;
+   * }
+   * ```
+   *
    * @static
    * @function
    * @param options {Object}
-   * `attributes` (Object) describes each attribute of the model with the following options:
-   * * `$get`
-   * * `$set`
-   * * `$validate`
+   * `attributes` (Object) describes each attribute of the model. See above for details.
    *
-   * `baseUrl` (String)
+   * `baseUrl` (String) will be interpolated with the model's attributes
    *
    * `idAttribute` (String) overrides the attribute used for {@link module:dougal.Model#$id|$id}
    *
-   * `initialize` (Function)
+   * `initialize` (Function) constructor inheritance
    * @memberof module:dougal
    * @returns {ExtendedModel}
    * @since 0.1.0
@@ -42,10 +74,16 @@ function extendModel(Collection, HttpStore, Model) {
 
     _.assign(ExtendedModel, {
       /**
-       * (soon)
+       * Retrieves all instances of the model
+       *
        * @static
        * @memberof module:dougal.Model
        * @returns {Promise} The promise resolves to an instance of {@link module:dougal.Collection|Collection}
+       * @example
+       * Car.all().then(function (cars) {
+       *   cars.length; // 1
+       *   instanceof cars[0]; // Car
+       * });
        */
       all: function () {
         return ExtendedModel.prototype.$$store.list({}).then(function (data) {
@@ -57,11 +95,19 @@ function extendModel(Collection, HttpStore, Model) {
       },
 
       /**
-       * (soon)
+       * Find an instance by ID
+       *
        * @static
        * @param id {any}
        * @memberof module:dougal.Model
        * @returns {Promise} The promise resolves to an instance of {@link module:dougal.Model|Model}
+       * @example
+       * Car.find(1).then(function (car) {
+       *   instanceof car; // Car
+       * });
+       * // Equivalent:
+       * var car = new Car({id: 1});
+       * car.$fetch();
        */
       find: function (id) {
         var model = new ExtendedModel();
@@ -70,10 +116,16 @@ function extendModel(Collection, HttpStore, Model) {
       },
 
       /**
-       * (soon)
+       * Searches for instances matching `options`
        * @static
+       * @param options {any}
        * @memberof module:dougal.Model
        * @returns {Promise} The promise resolves to an instance of {@link module:dougal.Collection|Collection}
+       * @example
+       * Car.where({some: 'filter'}).then(function (cars) {
+       *   cars.length; // 1
+       *   instanceof cars[0]; // Car
+       * });
        */
       where: function (options) {
         return ExtendedModel.prototype.$$store.list(options).then(function (data) {
